40fff60198972f2bdce7477d263c02724a90a1bc
efanov_evgeniy <efanovev1995@gmail.com>
2016-11-01 14:58:07 +0300

    Profile activity logic

--- app/src/main/java/com/chisto/Activities/ProfileActivity.java
+++ app/src/main/java/com/chisto/Activities/ProfileActivity.java
@@ -1,7 +1,9 @@
 package com.chisto.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.chisto.Base.BaseActivity;
 import com.chisto.R;
@@ -16,5 +18,44 @@ public class ProfileActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_profile);
+
+        findViewById(R.id.back_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        findViewById(R.id.contact_data_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, ContactInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.rules_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(ProfileActivity.this, ContactInfoActivity.class);
+//                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.my_orders_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, MyOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.about_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
