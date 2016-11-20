e6faa6487095fb5de6766744209a862f37d12061
efanov_evgeniy <efanovev1995@gmail.com>
2016-11-20 19:07:54 +0300

    Fix big constructor

--- app/src/main/java/ru/binaryblitz/Chisto/Model/MyOrder.kt
+++ app/src/main/java/ru/binaryblitz/Chisto/Model/MyOrder.kt
@@ -1,9 +1,51 @@
 package ru.binaryblitz.Chisto.Model
 
-import java.util.Date
+import com.google.gson.JsonObject
+import ru.binaryblitz.Chisto.Utils.AndroidUtilities
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MyOrder(var id: Int, var laundryId: Int, var isPaid: Boolean, var status: MyOrder.Status?, var createAt: Date?, var house: String?,
-              var street: String?, var flat: String?, var phone: String?, var note: String?) {
+class MyOrder {
+    val id: Int
+    val laundryId: Int
+    val isPaid: Boolean
+    val status: MyOrder.Status
+    val createAt: Date
+    val house: String
+    val street: String
+    val flat: String
+    val phone: String
+    val note: String
+
+    constructor(obj: JsonObject) {
+        id = AndroidUtilities.getIntFieldFromJson(obj.get("id"))
+        laundryId = AndroidUtilities.getIntFieldFromJson(obj.get("laundry_id"))
+        isPaid = AndroidUtilities.getBooleanFieldFromJson(obj.get("paid"))
+        status = getStatusFromJson(obj)
+        createAt = getDateFromJson(obj)!!
+        house = AndroidUtilities.getStringFieldFromJson(obj.get("house"))
+        street = AndroidUtilities.getStringFieldFromJson(obj.get("street"))
+        flat = AndroidUtilities.getStringFieldFromJson(obj.get("flat"))
+        phone = AndroidUtilities.getStringFieldFromJson(obj.get("contact_number"))
+        note = AndroidUtilities.getStringFieldFromJson(obj.get("notes"))
+    }
+
+    private fun getStatusFromJson(obj: JsonObject): MyOrder.Status {
+        return if (obj.get("status").asString == "processing") MyOrder.Status.PROCESS
+        else if (obj.get("status").asString == "completed") MyOrder.Status.COMPLETED else MyOrder.Status.CANCELED
+    }
+
+
+    private fun getDateFromJson(obj: JsonObject): Date? {
+        var date: Date? = null
+        try {
+            val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US)
+            format.timeZone = TimeZone.getTimeZone("UTC")
+            date = format.parse(obj.get("created_at").asString)
+        } catch (ignored: Exception) { }
+
+        return date
+    }
 
     enum class Status {
         PROCESS,
