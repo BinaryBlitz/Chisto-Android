d10cc4f46d7a63dc2537ce73f805ef425d9d2509
Dan Kim <itsdanya@gmail.com>
2016-11-21 19:07:12 +0300

    Screen design

--- app/src/main/java/ru/binaryblitz/Chisto/Activities/MapActivity.java
+++ app/src/main/java/ru/binaryblitz/Chisto/Activities/MapActivity.java
@@ -1,4 +1,28 @@
 package ru.binaryblitz.Chisto.Activities;
 
-public class MapActivity {
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ImageSpan;
+import android.widget.TextView;
+
+import ru.binaryblitz.Chisto.Base.BaseActivity;
+import ru.binaryblitz.Chisto.Custom.MyMapFragment;
+import ru.binaryblitz.Chisto.R;
+
+public class MapActivity extends BaseActivity implements MyMapFragment.TouchableWrapper.UpdateMapAfterUserInteraction {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+
+    @Override
+    public void onUpdateMapAfterUserInteraction() {
+
+    }
 }
