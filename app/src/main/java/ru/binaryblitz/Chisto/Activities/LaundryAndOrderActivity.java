71d2abe90138cad43eb1570f720828a4d4c7e395
efanov_evgeniy <efanovev1995@gmail.com>
2016-11-26 23:44:06 +0300

    Fix crash on review activity

--- app/src/main/java/ru/binaryblitz/Chisto/Activities/LaundryAndOrderActivity.java
+++ app/src/main/java/ru/binaryblitz/Chisto/Activities/LaundryAndOrderActivity.java
@@ -24,6 +24,7 @@ import ru.binaryblitz.Chisto.Server.DeviceInfoStore;
 import ru.binaryblitz.Chisto.Server.ServerApi;
 import ru.binaryblitz.Chisto.Server.ServerConfig;
 import ru.binaryblitz.Chisto.Utils.Image;
+import ru.binaryblitz.Chisto.Utils.LogUtil;
 
 public class LaundryAndOrderActivity extends BaseActivity {
 
