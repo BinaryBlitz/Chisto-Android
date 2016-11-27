355d04f698534ea1e8b9009b8469cf93293dda5f
efanov_evgeniy <efanovev1995@gmail.com>
2016-11-27 03:06:43 +0300

    Add phone call

--- app/src/main/java/ru/binaryblitz/Chisto/Activities/AboutActivity.java
+++ app/src/main/java/ru/binaryblitz/Chisto/Activities/AboutActivity.java
@@ -1,7 +1,14 @@
 package ru.binaryblitz.Chisto.Activities;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 
 import ru.binaryblitz.Chisto.Base.BaseActivity;
@@ -12,6 +19,8 @@ import io.fabric.sdk.android.Fabric;
 import ru.binaryblitz.Chisto.R;
 
 public class AboutActivity extends BaseActivity {
+
+    private static final int CALL_PERMISSION = 1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,5 +34,43 @@ public class AboutActivity extends BaseActivity {
                 finish();
             }
         });
+
+        findViewById(R.id.phone_call).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(AboutActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(AboutActivity.this, new String[]{Manifest.permission.CALL_PHONE}, CALL_PERMISSION);
+                } else {
+                    call();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CALL_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    call();
+                } else {
+                    onCallError();
+                }
+            }
+        }
+    }
+
+    private void onCallError() {
+        Snackbar.make(findViewById(R.id.main), R.string.call_error, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void call() {
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+7 999 111-22-33"));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            onCallError();
+            return;
+        }
+        startActivity(intent);
     }
 }
