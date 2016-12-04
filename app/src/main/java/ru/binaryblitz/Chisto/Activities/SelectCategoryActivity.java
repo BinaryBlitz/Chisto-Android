0132f5ffb9efb17ae70d25f11c9251970e552077
efanov_evgeniy <efanovev1995@gmail.com>
2016-12-04 21:47:44 +0300

    Implement last order button

--- app/src/main/java/ru/binaryblitz/Chisto/Activities/SelectCategoryActivity.java
+++ app/src/main/java/ru/binaryblitz/Chisto/Activities/SelectCategoryActivity.java
@@ -7,7 +7,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
