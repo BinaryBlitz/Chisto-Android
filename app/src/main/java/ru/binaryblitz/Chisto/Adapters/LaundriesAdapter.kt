4e18cb4f1f1049e15534f1b9bf9252379a110bbe
efanov_evgeniy <efanovev1995@gmail.com>
2016-12-04 20:20:17 +0300

    Laundries dates and costs

--- app/src/main/java/ru/binaryblitz/Chisto/Adapters/LaundriesAdapter.kt
+++ app/src/main/java/ru/binaryblitz/Chisto/Adapters/LaundriesAdapter.kt
@@ -15,6 +15,7 @@ import ru.binaryblitz.Chisto.Model.Laundry
 import ru.binaryblitz.Chisto.R
 import ru.binaryblitz.Chisto.Utils.Image
 import ru.binaryblitz.Chisto.Utils.OrderList
+import java.text.SimpleDateFormat
 import java.util.*
 
 class LaundriesAdapter(private val context: Activity) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -40,32 +41,10 @@ class LaundriesAdapter(private val context: Activity) : RecyclerView.Adapter<Rec
 
         holder.name.text = laundry.name
         holder.desc.text = laundry.desc
-
-        holder.category.visibility = View.VISIBLE
-        holder.categoryBack.visibility = View.VISIBLE
-
         holder.ratingBar.rating = laundry.rating
 
-        when (laundry.type) {
-            Laundry.Type.FAST -> {
-                holder.category.text = context.getString(R.string.premium)
-                holder.categoryBack.setImageResource(R.drawable.fast_bg)
-
-            }
-            Laundry.Type.PREMIUM -> {
-                holder.category.text = context.getString(R.string.fast)
-                holder.categoryBack.setImageResource(R.drawable.premium_bg)
-
-            }
-            Laundry.Type.ECONOMY -> {
-                holder.category.text = context.getString(R.string.economy)
-                holder.categoryBack.setImageResource(R.drawable.economy_bg)
-            }
-            Laundry.Type.EMPTY -> {
-                holder.category.visibility = View.GONE
-                holder.categoryBack.visibility = View.GONE
-            }
-        }
+        setDatesAndCosts(laundry, holder)
+        setCategory(laundry, holder)
 
         Image.loadPhoto(laundry.icon, holder.icon)
 
@@ -76,6 +55,50 @@ class LaundriesAdapter(private val context: Activity) : RecyclerView.Adapter<Rec
             intent.putExtra(EXTRA_ID, laundry.id)
             context.startActivity(intent)
         }
+    }
+
+    private fun setDatesAndCosts(laundry: Laundry, holder: ViewHolder) {
+        holder.collectionCost.text = laundry.deliveryCost.toString() + " \u20bd"
+        holder.collectionDate.text = getDateStringRepresentationWithoutTime(laundry.collectionDate!!)
+        holder.deliveryDate.text = getDateStringRepresentationWithoutTime(laundry.deliveryDate!!)
+        holder.deliveryBounds.text =
+                context.getString(R.string.from_code) + getTimeStringRepresentation(laundry.deliveryDateOpensAt!!) +
+                        context.getString(R.string.end_bound_code) +
+                        getTimeStringRepresentation(laundry.deliveryDateClosesAt!!)
+        holder.cost.text = laundry.orderCost.toString() + " \u20bd"
+    }
+
+    private fun setCategory(laundry: Laundry, holder: ViewHolder) {
+        when (laundry.type) {
+            Laundry.Type.FAST -> {
+                holder.category.text = context.getString(R.string.premium)
+                holder.categoryBack.setImageResource(R.drawable.fast_bg)
+            }
+            Laundry.Type.PREMIUM -> {
+                holder.category.text = context.getString(R.string.fast)
+                holder.categoryBack.setImageResource(R.drawable.premium_bg)
+            }
+            Laundry.Type.ECONOMY -> {
+                holder.category.text = context.getString(R.string.economy)
+                holder.categoryBack.setImageResource(R.drawable.economy_bg)
+            }
+            Laundry.Type.EMPTY -> {
+                holder.category.visibility = View.GONE
+                holder.categoryBack.visibility = View.GONE
+            }
+        }
+    }
+
+    fun getDateStringRepresentationWithoutTime(date: Date): String {
+        val format = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
+        format.timeZone = TimeZone.getDefault()
+        return format.format(date)
+    }
+
+    fun getTimeStringRepresentation(date: Date): String {
+        val format = SimpleDateFormat("HH:mm", Locale.getDefault())
+        format.timeZone = TimeZone.getDefault()
+        return format.format(date)
     }
 
     override fun getItemCount(): Int {
@@ -90,6 +113,11 @@ class LaundriesAdapter(private val context: Activity) : RecyclerView.Adapter<Rec
         val name: TextView
         val desc: TextView
         val category: TextView
+        val collectionDate: TextView
+        val collectionCost: TextView
+        val deliveryDate: TextView
+        val deliveryBounds: TextView
+        val cost: TextView
         val icon: ImageView
         val categoryBack: ImageView
         val ratingBar: SimpleRatingBar
@@ -98,6 +126,11 @@ class LaundriesAdapter(private val context: Activity) : RecyclerView.Adapter<Rec
             name = itemView.findViewById(R.id.name) as TextView
             desc = itemView.findViewById(R.id.description) as TextView
             category = itemView.findViewById(R.id.type_text) as TextView
+            collectionDate = itemView.findViewById(R.id.curier_date) as TextView
+            collectionCost = itemView.findViewById(R.id.curier_cost) as TextView
+            deliveryDate = itemView.findViewById(R.id.delivery_date) as TextView
+            deliveryBounds = itemView.findViewById(R.id.delivery_bounds) as TextView
+            cost = itemView.findViewById(R.id.sum) as TextView
             icon = itemView.findViewById(R.id.category_icon) as ImageView
             categoryBack = itemView.findViewById(R.id.type) as ImageView
             ratingBar = itemView.findViewById(R.id.ratingBar) as SimpleRatingBar
