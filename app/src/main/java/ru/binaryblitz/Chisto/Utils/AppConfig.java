f371e7954ebb12b4d96f1aa389e34c209863ed1f
efanov_evgeniy <efanovev1995@gmail.com>
2016-12-14 03:16:24 +0300

    Fix instagram url

--- app/src/main/java/ru/binaryblitz/Chisto/Utils/AppConfig.java
+++ app/src/main/java/ru/binaryblitz/Chisto/Utils/AppConfig.java
@@ -6,7 +6,7 @@ public class AppConfig {
 
     public static final String baseEmail = "info@chis.to";
     public static final String partnerEmail = "partner@chis.to";
-    public static final String instagram = "https://instagram.com/chistoapp";
+    public static final String instagram = "https://www.instagram.com/chistoapp/";
     public static final String facebook = "https://www.facebook.com/chistoapp";
     public static final String vk = "https://vk.com/chistoapp";
     public static final String googlePlay = "https://play.google.com/store/apps/details?id=ru.binaryblitz.Chisto";
