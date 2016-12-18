c39fe548552b99ecaf68775ab30790ad34d7006b
efanov_evgeniy <efanovev1995@gmail.com>
2016-12-18 21:11:40 +0300

    Fix size dialog

--- app/src/main/java/ru/binaryblitz/Chisto/Activities/SelectServiceActivity.kt
+++ app/src/main/java/ru/binaryblitz/Chisto/Activities/SelectServiceActivity.kt
@@ -148,7 +148,7 @@ class SelectServiceActivity : BaseActivity() {
         try {
             if (editable.isNotEmpty() && editable.toString() != "0") {
                 if (width) this.width = Integer.parseInt(editable.toString()) else length = Integer.parseInt(editable.toString())
-                square.text = java.lang.Double.toString((length * this.width).toDouble() / 1000.0) + " м²"
+                square.text = java.lang.Double.toString((length * this.width).toDouble() / 10000.0) + " м²"
             }
         } catch (e: Exception) {
             LogUtil.logException(e)
