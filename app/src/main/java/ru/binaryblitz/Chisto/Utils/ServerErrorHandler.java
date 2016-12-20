70295e22fcd402a820f136532da01a16037cf74c
efanov_evgeniy <efanovev1995@gmail.com>
2016-12-20 22:42:51 +0300

    Add default text to server error

--- app/src/main/java/ru/binaryblitz/Chisto/Utils/ServerErrorHandler.java
+++ app/src/main/java/ru/binaryblitz/Chisto/Utils/ServerErrorHandler.java
@@ -37,11 +37,17 @@ public class ServerErrorHandler {
         } catch (IOException e) {
             LogUtil.logException(e);
             error = new APIError();
-            // невозможно получить доступ к контексту, поэтому нельзя извлечь в файл /strings
-            error.message = "Неверно введены данные";
-            error.statusCode = 0;
+            setDefault(error);
         }
 
+        if (error.message.isEmpty()) setDefault(error);
+
         return error;
+    }
+
+    private static void setDefault(APIError error) {
+        // невозможно получить доступ к контексту, поэтому нельзя извлечь в файл /strings
+        error.message = "Неверно введены данные";
+        error.statusCode = 0;
     }
 }
