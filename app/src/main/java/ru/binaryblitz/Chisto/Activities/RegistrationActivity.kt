4fdc42735968753134a19579475129c7e14f3ff7
efanov_evgeniy <efanovev1995@gmail.com>
2017-01-10 15:13:29 +0300

    Fix category item UI

--- app/src/main/java/ru/binaryblitz/Chisto/Activities/RegistrationActivity.kt
+++ app/src/main/java/ru/binaryblitz/Chisto/Activities/RegistrationActivity.kt
@@ -67,11 +67,9 @@ class RegistrationActivity : BaseActivity() {
         phoneEditText!!.addTextChangedListener(PhoneNumberFormattingTextWatcher())
 
         codeEditText!!.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
-            }
+            override fun afterTextChanged(p0: Editable?) { }
 
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 if (p0!!.length == 5) verifyRequest()
