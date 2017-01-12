ced3ce82fa308ece3ecfaff1e6551991babedcf8
efanov_evgeniy <efanovev1995@gmail.com>
2017-01-12 15:04:05 +0300

    Move new-order-popup to OrdersActivity

--- app/src/main/java/ru/binaryblitz/Chisto/Activities/PersonalInfoActivity.kt
+++ app/src/main/java/ru/binaryblitz/Chisto/Activities/PersonalInfoActivity.kt
@@ -48,8 +48,6 @@ class PersonalInfoActivity : BaseActivity() {
 
     private var user: User? = null
 
-    private var dialogOpened = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Fabric.with(this, Crashlytics())
@@ -108,14 +106,15 @@ class PersonalInfoActivity : BaseActivity() {
         finishActivity()
     }
 
+    private fun showPaymentError() {
+        Snackbar.make(findViewById(R.id.main), getString(R.string.payment_error), Snackbar.LENGTH_SHORT).show()
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK && requestCode == REQUEST_WEB) {
-            if (data!!.getBooleanExtra("success", false)) {
-                showOrderDialog(orderId)
-            } else {
-                Snackbar.make(findViewById(R.id.main), getString(R.string.payment_error), Snackbar.LENGTH_SHORT).show()
-            }
+            if (data!!.getBooleanExtra("success", false)) complete(orderId)
+            else showPaymentError()
         }
     }
 
@@ -191,7 +190,7 @@ class PersonalInfoActivity : BaseActivity() {
     private fun parseAnswer(obj: JsonObject, payWithCreditCard: Boolean) {
         orderId = obj.get("id").asInt
         if (payWithCreditCard) openWebActivity(obj.get("payment").asJsonObject.get("payment_url").asString)
-        else showOrderDialog(orderId)
+        else complete(orderId)
     }
 
     private fun openWebActivity(url: String) {
@@ -200,20 +199,13 @@ class PersonalInfoActivity : BaseActivity() {
         startActivityForResult(intent, REQUEST_WEB)
     }
 
-    private fun showOrderDialog(id: Int) {
-        Handler().post {
-            dialogOpened = true
-            (findViewById(R.id.order_name) as TextView).text = "№ " + id.toString()
-            Animations.animateRevealShow(findViewById(ru.binaryblitz.Chisto.R.id.dialog), this@PersonalInfoActivity)
-        }
+    private fun complete(orderId: Int) {
+        OrdersActivity.newOrderId = orderId
+        OrderList.clear()
+        goToOrderActivity()
     }
 
     private fun setOnClickListeners() {
-        findViewById(R.id.cont_btn).setOnClickListener {
-            OrderList.clear()
-            goToOrderActivity()
-        }
-
         findViewById(R.id.left_btn).setOnClickListener { finishActivity() }
 
         findViewById(R.id.pay_btn).setOnClickListener { process(false) }
@@ -250,7 +242,7 @@ class PersonalInfoActivity : BaseActivity() {
         email = findViewById(R.id.email) as MaterialEditText
         phone!!.addTextChangedListener(PhoneNumberFormattingTextWatcher())
 
-        (findViewById(R.id.price) as TextView).text = intent.getIntExtra(EXTRA_PRICE, 0).toString() + " \u20bd"
+        (findViewById(R.id.price) as TextView).text = intent.getIntExtra(EXTRA_PRICE, 0).toString() + getString(R.string.ruble_sign)
     }
 
     private fun setInfo() {
