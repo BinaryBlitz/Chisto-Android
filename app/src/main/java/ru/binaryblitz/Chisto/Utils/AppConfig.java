1cea8823d70ccbdfacaeacc11cad23b09b3f362e
Dan Kim <itsdanya@gmail.com>
2017-01-16 18:50:02 +0300

    Add terms to registration

--- app/src/main/java/ru/binaryblitz/Chisto/Utils/AppConfig.java
+++ app/src/main/java/ru/binaryblitz/Chisto/Utils/AppConfig.java
@@ -10,6 +10,7 @@ public class AppConfig {
     public static final String facebook = "https://www.facebook.com/chistoapp";
     public static final String vk = "https://vk.com/chistoapp";
     public static final String googlePlay = "https://play.google.com/store/apps/details?id=ru.binaryblitz.Chisto";
+    public static final String terms = "https://docs.google.com/viewer?url=" + "https://chis.to/legal/terms-of-service.pdf";
 
     public static final int decorationId = -1;
 }
