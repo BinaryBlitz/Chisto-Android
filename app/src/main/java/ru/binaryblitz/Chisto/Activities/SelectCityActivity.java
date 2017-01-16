820ba77cafb6122896f63761d24a395a6225e5e2
efanov_evgeniy <efanovev1995@gmail.com>
2017-01-16 13:07:13 +0300

    Apply new formatting

--- app/src/main/java/ru/binaryblitz/Chisto/Activities/SelectCityActivity.java
+++ app/src/main/java/ru/binaryblitz/Chisto/Activities/SelectCityActivity.java
@@ -46,6 +46,7 @@ import ru.binaryblitz.Chisto.Model.City;
 import ru.binaryblitz.Chisto.R;
 import ru.binaryblitz.Chisto.Server.ServerApi;
 import ru.binaryblitz.Chisto.Utils.AndroidUtilities;
+import ru.binaryblitz.Chisto.Utils.CustomPhoneNumberTextWatcher;
 import ru.binaryblitz.Chisto.Utils.LogUtil;
 
 public class SelectCityActivity extends BaseActivity
@@ -100,7 +101,7 @@ public class SelectCityActivity extends BaseActivity
         if (view == null) return;
         phone = (MaterialEditText) view.findViewById(R.id.editText);
         city = (MaterialEditText) view.findViewById(R.id.editText2);
-        phone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+        phone.addTextChangedListener(new CustomPhoneNumberTextWatcher());
     }
 
     @Override
