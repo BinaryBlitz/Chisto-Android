cb2f81fa0e85b8ea0fa741dfe0ba36a074fb8f59
efanov_evgeniy <efanovev1995@gmail.com>
2017-01-16 14:53:48 +0300

    Add status text to order items

--- app/src/main/java/ru/binaryblitz/Chisto/Adapters/MyOrdersAdapter.kt
+++ app/src/main/java/ru/binaryblitz/Chisto/Adapters/MyOrdersAdapter.kt
@@ -34,6 +34,8 @@ class MyOrdersAdapter(private val context: Activity) : RecyclerView.Adapter<Recy
         setIconAndColor(order, holder)
 
         holder.desc.text = getDateStringRepresentation(order.createAt)
+        holder.status.text = getDateStringRepresentation(order.createAt)
+
         holder.cost.text = order.price.toString() + context.getString(R.string.ruble_sign)
 
         holder.itemView.setOnClickListener {
@@ -46,30 +48,31 @@ class MyOrdersAdapter(private val context: Activity) : RecyclerView.Adapter<Recy
     private fun setIconAndColor(order: MyOrder, holder: ViewHolder) {
         when (order.status) {
             MyOrder.Status.COMPLETED -> {
-                holder.marker.setImageResource(R.drawable.ic_completed_indicator)
-                holder.cost.setTextColor(ContextCompat.getColor(context, R.color.completedColor))
+                setInformation(holder, R.drawable.ic_completed_indicator, R.color.completedColor, R.string.ready_code)
             }
             MyOrder.Status.CANCELED -> {
-                holder.marker.setImageResource(R.drawable.ic_canceled_indicator)
-                holder.cost.setTextColor(ContextCompat.getColor(context, R.color.canceledColor))
+                setInformation(holder, R.drawable.ic_canceled_indicator, R.color.canceledColor, R.string.canceled_code)
             }
             MyOrder.Status.CONFIRMED -> {
-                holder.marker.setImageResource(R.drawable.ic_confirmed_indicator)
-                holder.cost.setTextColor(ContextCompat.getColor(context, R.color.confirmedColor))
+                setInformation(holder, R.drawable.ic_confirmed_indicator, R.color.confirmedColor, R.string.confirmed_code)
             }
             MyOrder.Status.DISPATCHED -> {
-                holder.marker.setImageResource(R.drawable.ic_dispatched_indicator)
-                holder.cost.setTextColor(ContextCompat.getColor(context, R.color.dispatchedColor))
+                setInformation(holder, R.drawable.ic_dispatched_indicator, R.color.dispatchedColor, R.string.dispatched_code)
             }
             MyOrder.Status.CLEANING -> {
-                holder.marker.setImageResource(R.drawable.ic_cleaning_indicator)
-                holder.cost.setTextColor(ContextCompat.getColor(context, R.color.cleaningColor))
+                setInformation(holder, R.drawable.ic_cleaning_indicator, R.color.cleaningColor, R.string.cleaning_code)
             }
             else -> {
-                holder.cost.setTextColor(ContextCompat.getColor(context, R.color.processColor))
-                holder.marker.setImageResource(R.drawable.ic_process_indicator)
+                setInformation(holder, R.drawable.ic_process_indicator, R.color.processColor, R.string.process_code)
             }
         }
+    }
+
+    private fun setInformation(holder: ViewHolder, icon: Int, color: Int, text: Int) {
+        holder.marker.setImageResource(icon)
+        holder.cost.setTextColor(ContextCompat.getColor(context, color))
+        holder.status.setTextColor(ContextCompat.getColor(context, color))
+        holder.status.text = context.getString(text)
     }
 
     fun getDateStringRepresentation(date: Date): String {
@@ -88,16 +91,11 @@ class MyOrdersAdapter(private val context: Activity) : RecyclerView.Adapter<Recy
     }
 
     private inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val name: TextView
-        val cost: TextView
-        val desc: TextView
-        val marker: ImageView
+        val name: TextView = itemView.findViewById(R.id.name) as TextView
+        val cost: TextView = itemView.findViewById(R.id.cost) as TextView
+        val desc: TextView = itemView.findViewById(R.id.desc) as TextView
+        val status: TextView = itemView.findViewById(R.id.status) as TextView
+        val marker: ImageView = itemView.findViewById(R.id.marker) as ImageView
 
-        init {
-            name = itemView.findViewById(R.id.name) as TextView
-            cost = itemView.findViewById(R.id.cost) as TextView
-            desc = itemView.findViewById(R.id.desc) as TextView
-            marker = itemView.findViewById(R.id.marker) as ImageView
-        }
     }
 }
