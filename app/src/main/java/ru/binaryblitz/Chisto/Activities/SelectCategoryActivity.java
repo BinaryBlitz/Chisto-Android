f5c57247e8765bb853c27b6608349c5dd09050f5
outboard <efanovev1995@gmail.com>
2017-02-22 12:51:49 +0300

    Remove voice search callback

--- app/src/main/java/ru/binaryblitz/Chisto/Activities/SelectCategoryActivity.java
+++ app/src/main/java/ru/binaryblitz/Chisto/Activities/SelectCategoryActivity.java
@@ -134,22 +134,6 @@ public class SelectCategoryActivity extends BaseActivity {
         return true;
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == RESULT_OK) {
-            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            if (matches != null && matches.size() > 0) {
-                String searchWrd = matches.get(0);
-                if (!TextUtils.isEmpty(searchWrd)) {
-                    searchView.setQuery(searchWrd, false);
-                }
-            }
-
-            return;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
     private void initList() {
         RecyclerListView view = (RecyclerListView) findViewById(ru.binaryblitz.Chisto.R.id.recyclerView);
         view.setLayoutManager(new LinearLayoutManager(this));
