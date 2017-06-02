e586c83e01e3857eaca494c7c3b014b75942dc7a
Ilia Savin <savinilya34@gmail.com>
2017-06-02 18:44:59 +0600

    Implement screen navigation

--- app/src/main/java/ru/binaryblitz/Chisto/ui/start/StartActivity.java
+++ app/src/main/java/ru/binaryblitz/Chisto/ui/start/StartActivity.java
@@ -1,10 +1,14 @@
 package ru.binaryblitz.Chisto.ui.start;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.crashlytics.android.Crashlytics;
 import com.rd.PageIndicatorView;
@@ -19,7 +23,13 @@ import ru.binaryblitz.Chisto.ui.start.onboarding.ThirdPageFragment;
 import ru.binaryblitz.Chisto.ui.start.onboarding.WelcomePageFragment;
 import ru.binaryblitz.Chisto.ui.start.onboarding.base.OnBoardingFragment;
 
-public class StartActivity extends BaseActivity implements ViewPager.OnPageChangeListener {
+
+public class StartActivity extends BaseActivity implements ViewPager.OnPageChangeListener,
+        View.OnClickListener {
+    PageIndicatorView pageIndicatorView;
+    Button nextBtn;
+    ImageView skipBtn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,13 +71,24 @@ public class StartActivity extends BaseActivity implements ViewPager.OnPageChang
         viewPager.addOnPageChangeListener(thirdPageFragment);
         viewPager.addOnPageChangeListener(this);
 
-        PageIndicatorView pageIndicatorView = (PageIndicatorView) findViewById(R.id.pageIndicatorView);
+        nextBtn = (Button) findViewById(R.id.nextBtn);
+        skipBtn = (ImageView) findViewById(R.id.skip_onboarding);
+        pageIndicatorView = (PageIndicatorView) findViewById(R.id.pageIndicatorView);
         pageIndicatorView.setViewPager(viewPager);
         pageIndicatorView.setRadius(4);
         pageIndicatorView.setAnimationType(AnimationType.SLIDE);
         pageIndicatorView.setSelectedColor(getResources().getColor(R.color.colorPrimary));
         pageIndicatorView.setUnselectedColor(getResources().getColor(R.color.textColor));
 
+        nextBtn.setOnClickListener(this);
+        skipBtn.setOnClickListener(this);
+
+    }
+
+    private void openSelectCityScreen() {
+        Intent openSelectCityScreenIntent = new Intent(StartActivity.this, SelectCityActivity.class);
+        startActivity(openSelectCityScreenIntent);
+        finish();
     }
 
     @Override
@@ -76,9 +97,26 @@ public class StartActivity extends BaseActivity implements ViewPager.OnPageChang
 
     @Override
     public void onPageSelected(int position) {
+        if (position == 3) {
+            pageIndicatorView.setVisibility(View.GONE);
+            nextBtn.setVisibility(View.VISIBLE);
+            return;
+        }
+        pageIndicatorView.setVisibility(View.VISIBLE);
+        nextBtn.setVisibility(View.GONE);
+
     }
+
 
     @Override
     public void onPageScrollStateChanged(int state) {
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.nextBtn || v.getId() == R.id.skip_onboarding)
+            openSelectCityScreen();
+    }
 }
+
+
