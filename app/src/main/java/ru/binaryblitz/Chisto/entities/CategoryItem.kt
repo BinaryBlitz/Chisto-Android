ff34fc646df9c89b9b16cc117987cdd7662e01b5
Ilia Savin <savinilya34@gmail.com>
2017-06-09 00:16:02 +0600

    Fix code style in data classes

--- app/src/main/java/ru/binaryblitz/Chisto/entities/CategoryItem.kt
+++ app/src/main/java/ru/binaryblitz/Chisto/entities/CategoryItem.kt
@@ -5,46 +5,46 @@ import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class CategoryItem(
-        @SerializedName("id")
-        val id: Int,
-        @SerializedName("icon_url")
-        val icon: String,
-        @SerializedName("name")
-        val name: String,
-        @SerializedName("description")
-        val description: String,
-        @SerializedName("use_area")
-        val userArea: Boolean,
-        val color: Int,
-        @SerializedName("long_treatment")
-        val isLongTreatment: Boolean) : Parcelable {
-        companion object {
-                @JvmField val CREATOR: Parcelable.Creator<CategoryItem> = object : Parcelable.Creator<CategoryItem> {
-                        override fun createFromParcel(source: Parcel): CategoryItem = CategoryItem(source)
-                        override fun newArray(size: Int): Array<CategoryItem?> = arrayOfNulls(size)
-                }
-        }
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("icon_url")
+    val icon: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("use_area")
+    val userArea: Boolean,
+    val color: Int,
+    @SerializedName("long_treatment")
+    val isLongTreatment: Boolean) : Parcelable {
+  companion object {
+    @JvmField val CREATOR: Parcelable.Creator<CategoryItem> = object : Parcelable.Creator<CategoryItem> {
+      override fun createFromParcel(source: Parcel): CategoryItem = CategoryItem(source)
+      override fun newArray(size: Int): Array<CategoryItem?> = arrayOfNulls(size)
+    }
+  }
 
-        constructor(source: Parcel) : this(
-        source.readInt(),
-        source.readString(),
-        source.readString(),
-        source.readString(),
-        1 == source.readInt(),
-        source.readInt(),
-        1 == source.readInt()
-        )
+  constructor(source: Parcel) : this(
+      source.readInt(),
+      source.readString(),
+      source.readString(),
+      source.readString(),
+      1 == source.readInt(),
+      source.readInt(),
+      1 == source.readInt()
+  )
 
-        override fun describeContents() = 0
+  override fun describeContents() = 0
 
-        override fun writeToParcel(dest: Parcel, flags: Int) {
-                dest.writeInt(id)
-                dest.writeString(icon)
-                dest.writeString(name)
-                dest.writeString(description)
-                dest.writeInt((if (userArea) 1 else 0))
-                dest.writeInt(color)
-                dest.writeInt((if (isLongTreatment) 1 else 0))
-        }
+  override fun writeToParcel(dest: Parcel, flags: Int) {
+    dest.writeInt(id)
+    dest.writeString(icon)
+    dest.writeString(name)
+    dest.writeString(description)
+    dest.writeInt((if (userArea) 1 else 0))
+    dest.writeInt(color)
+    dest.writeInt((if (isLongTreatment) 1 else 0))
+  }
 }
 
--- app/src/main/java/ru/binaryblitz/Chisto/entities/Order.kt
+++ app/src/main/java/ru/binaryblitz/Chisto/entities/Order.kt
@@ -2,38 +2,40 @@ package ru.binaryblitz.Chisto.entities
 
 import android.os.Parcel
 import android.os.Parcelable
-import java.util.*
+import java.util.ArrayList
 
-data class Order(val category: CategoryItem, var treatments: ArrayList<Treatment>?, var count: Int, var color: Int,
-                 val decoration: Boolean, var decorationPrice: Int, var size: Double?, var hasItemsWithLongTreatment: Boolean) : Parcelable {
-    companion object {
-        @JvmField val CREATOR: Parcelable.Creator<Order> = object : Parcelable.Creator<Order> {
-            override fun createFromParcel(source: Parcel): Order = Order(source)
-            override fun newArray(size: Int): Array<Order?> = arrayOfNulls(size)
-        }
+data class Order(val category: CategoryItem, var treatments: ArrayList<Treatment>?, var count: Int,
+    var color: Int,
+    val decoration: Boolean, var decorationPrice: Int, var size: Double?,
+    var hasItemsWithLongTreatment: Boolean) : Parcelable {
+  companion object {
+    @JvmField val CREATOR: Parcelable.Creator<Order> = object : Parcelable.Creator<Order> {
+      override fun createFromParcel(source: Parcel): Order = Order(source)
+      override fun newArray(size: Int): Array<Order?> = arrayOfNulls(size)
     }
+  }
 
-    constructor(source: Parcel) : this(
-            source.readParcelable<CategoryItem>(CategoryItem::class.java.classLoader),
-            ArrayList<Treatment>().apply { source.readList(this, Treatment::class.java.classLoader) },
-            source.readInt(),
-            source.readInt(),
-            1 == source.readInt(),
-            source.readInt(),
-            source.readValue(Double::class.java.classLoader) as Double?,
-            1 == source.readInt()
-    )
+  constructor(source: Parcel) : this(
+      source.readParcelable<CategoryItem>(CategoryItem::class.java.classLoader),
+      ArrayList<Treatment>().apply { source.readList(this, Treatment::class.java.classLoader) },
+      source.readInt(),
+      source.readInt(),
+      1 == source.readInt(),
+      source.readInt(),
+      source.readValue(Double::class.java.classLoader) as Double?,
+      1 == source.readInt()
+  )
 
-    override fun describeContents() = 0
+  override fun describeContents() = 0
 
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeParcelable(category, 0)
-        dest.writeList(treatments)
-        dest.writeInt(count)
-        dest.writeInt(color)
-        dest.writeInt((if (decoration) 1 else 0))
-        dest.writeInt(decorationPrice)
-        dest.writeValue(size)
-        dest.writeInt((if (hasItemsWithLongTreatment) 1 else 0))
-    }
+  override fun writeToParcel(dest: Parcel, flags: Int) {
+    dest.writeParcelable(category, 0)
+    dest.writeList(treatments)
+    dest.writeInt(count)
+    dest.writeInt(color)
+    dest.writeInt((if (decoration) 1 else 0))
+    dest.writeInt(decorationPrice)
+    dest.writeValue(size)
+    dest.writeInt((if (hasItemsWithLongTreatment) 1 else 0))
+  }
 }
