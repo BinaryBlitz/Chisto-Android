58d55b7276bde6f4ae30387d29742a8d0ceda128
eugenetereshkov <eugenetereshkov@ya.ru>
2017-12-06 02:51:07 +0300

    Refactoring OrderSimpleContentAdapter

--- app/src/main/java/ru/binaryblitz/Chisto/ui/order/adapters/OrderSimpleContentAdapter.kt
+++ app/src/main/java/ru/binaryblitz/Chisto/ui/order/adapters/OrderSimpleContentAdapter.kt
@@ -1,15 +1,16 @@
 package ru.binaryblitz.Chisto.ui.order.adapters
 
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_order_content_header.*
 import ru.binaryblitz.Chisto.R
+import ru.binaryblitz.Chisto.extension.inflate
 import ru.binaryblitz.Chisto.utils.Image
 
 
+//Adapter without subitems order info
 class OrderSimpleContentAdapter : RecyclerView.Adapter<OrderSimpleContentAdapter.ViewHolder>() {
 
     data class Item(val name: String, val sum: Int, val count: Int, val icon: String, val color: Int)
@@ -22,10 +23,8 @@ class OrderSimpleContentAdapter : RecyclerView.Adapter<OrderSimpleContentAdapter
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_order_content_header, parent, false)
-        return ViewHolder(view)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+            ViewHolder(parent.inflate(R.layout.item_order_content_header))
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(items[position])
@@ -38,11 +37,12 @@ class OrderSimpleContentAdapter : RecyclerView.Adapter<OrderSimpleContentAdapter
     ) : RecyclerView.ViewHolder(containerView), LayoutContainer {
 
         fun bind(item: Item) {
-            Image.loadPhoto(itemView.context, item.icon, category_icon)
-            category_icon.setColorFilter(item.color)
-            name.text = item.name + " " + (item.sum / item.count).toString() +
-                    itemView.context.getString(R.string.ruble_sign) + "  \u00D7" + item.count.toString()
-            price.text = item.sum.toString() + itemView.context.getString(R.string.ruble_sign)
+            itemView.context?.run {
+                Image.loadPhoto(this, item.icon, category_icon)
+                category_icon.setColorFilter(item.color)
+                name.text = "${item.name} ${item.sum / item.count} ${getString(R.string.ruble_sign)} \u00D7 ${item.count}"
+                price.text = "${item.sum} ${getString(R.string.ruble_sign)}"
+            }
         }
     }
 }
